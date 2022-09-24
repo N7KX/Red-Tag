d6aff9ea07b6df65d3cabff60e1e97a9c3037fa9
N7KX <109846517+N7KX@users.noreply.github.com>
2022-09-24 20:22:16 +0530

    Add files via upload

---
+++ Book_owner.java
@@ -0,0 +1,43 @@
+package SQL_Connector;
+
+import java.sql.Connection;
+
+import java.sql.ResultSet;
+
+import java.sql.Statement;
+
+public class Book_owner 
+{
+    public static int book_data_load(int ID,int UID)
+    {
+        Connection connection = Connector.connection();
+        
+        String SQL = "SELECT * FROM LEND_TABLE WHERE BOOK_ID = "+ID+" AND USER_ID = "+UID+";";
+        
+        int value = 0;
+        
+        try
+        {
+            Statement stmt = connection.createStatement();
+            
+            ResultSet rs = stmt.executeQuery(SQL);
+            
+            if(!rs.next())
+            {
+                value = 0;
+            }
+            else
+            {
+                value = 1;
+            }
+        }
+        catch(Exception ERROR)
+        {
+            
+        }
+        
+        return value;
+    }  
+
+    
+}
